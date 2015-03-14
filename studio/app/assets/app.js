3e0be38d2f11d1ccbb196e219f406961b57a88dc
Joshua Kidd <joshuabkidd@gmail.com>
2015-03-14 14:23:52 -0400

    added angular js play application

---
+++ studio/app/assets/app.js
@@ -0,0 +1,11 @@
+
+var analyticsApp = angular.module('analyticsApp', ['ngRoute', 'ngSanitize'])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/studio', {
+            templateUrl: "/assets/templates/studio.html"
+        });
+        $routeProvider.otherwise({ redirectTo: '/studio' });
+        $locationProvider.html5Mode({
+            enabled: true
+        });
+    });
