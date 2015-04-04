df551f321e52ccf1c607945c57d3a2eb82f5f430
Alper Enver <alper.enver@emerson.com>
2015-04-04 11:25:19 -0500

    test to see if project is bound to source control.

--- models/src/main/java/emr/analytics/models/diagram/Diagram.java
+++ models/src/main/java/emr/analytics/models/diagram/Diagram.java
@@ -154,4 +154,6 @@ public class Diagram implements Serializable {
                 .filter(w -> w.getTo_node().equals(name))
                 .collect(Collectors.toList());
     }
+
+    /* version control test */
 }
