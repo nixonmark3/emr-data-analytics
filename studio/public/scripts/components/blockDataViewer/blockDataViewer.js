922cbc25f9e735ebe02803555d1f398c6c3db0db
Joshua Kidd <joshuabkidd@gmail.com>
2015-04-24 08:03:11 -0500

    block data style updates.

--- studio/public/scripts/components/blockDataViewer/blockDataViewer.js
+++ studio/public/scripts/components/blockDataViewer/blockDataViewer.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var blockDataViewerApp = angular.module('blockDataViewerApp', []).directive('blockDataViewer', function () {
+var blockDataViewerApp = angular.module('blockDataViewerApp', []).directive('blockDataViewer', function ($timeout) {
 
         return {
             restrict: 'E',
@@ -38,71 +38,24 @@ var blockDataViewerApp = angular.module('blockDataViewerApp', []).directive('blo
                     }
                 };
 
-
-
-                /*$scope.pages = [{name: 'Statistics', data: [{"name": "630_MASS_FRAC_C5",
-                    "std" : 0.1512856681655764,
-                    "mean" : 0.2757579020697675,
-                    "min" : 0.09067086000000001,
-                    "fifty" : 0.222435572,
-                    "count" : 43,
-                    "max" : 0.571690008,
-                    "twentyFive" : 0.163085218,
-                    "seventyFive" : 0.403336692},
-                    {"name" : "Pressure",
-                        "std" : 3.304426112926163,
-                        "mean" : 18.85407301286819,
-                        "min" : 7.99486,
-                        "fifty" : 19.99857,
-                        "count" : 628993,
-                        "max" : 27.39091,
-                        "twentyFive" : 19.96193,
-                        "seventyFive" : 20.00355},
-                    {"name" : "PressureA",
-                        "std" : 3.304426112926163,
-                        "mean" : 18.85407301286819,
-                        "min" : 7.99486,
-                        "fifty" : 19.99857,
-                        "count" : 628993,
-                        "max" : 27.39091,
-                        "twentyFive" : 19.96193,
-                        "seventyFive" : 20.00355},
-                    {"name" : "PressureB",
-                        "std" : 3.304426112926163,
-                        "mean" : 18.85407301286819,
-                        "min" : 7.99486,
-                        "fifty" : 19.99857,
-                        "count" : 628993,
-                        "max" : 27.39091,
-                        "twentyFive" : 19.96193,
-                        "seventyFive" : 20.00355},
-                    {"name" : "PressureC",
-                        "std" : 3.304426112926163,
-                        "mean" : 18.85407301286819,
-                        "min" : 7.99486,
-                        "fifty" : 19.99857,
-                        "count" : 628993,
-                        "max" : 27.39091,
-                        "twentyFive" : 19.96193,
-                        "seventyFive" : 20.00355}] },
-                    {name: 'Plot', data: null },
-                    {name: 'Results', data: null }];*/
-
                 var init = function(){
 
-                    $scope.getBlockData('Pages',
-                        $scope.block.name,
-                        function(results){
+                    $timeout(function(){
 
-                            $scope.pages = [];
-                            for(var i = 0; i < results.length; i++){
-                                $scope.pages.push({name: results[i], data: null});
-                            }
+                        $scope.getBlockData('Pages',
+                            $scope.block.name,
+                            function(results){
 
-                            setPageName();
+                                $scope.pages = [];
+                                for(var i = 0; i < results.length; i++){
+                                    $scope.pages.push({name: results[i], data: null});
+                                }
 
-                            getData();
-                        });
+                                setPageName();
+
+                                getData();
+                            });
+                    }, 500);
 
                 };
 
