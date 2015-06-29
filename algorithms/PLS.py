d811b32937c592160c6ddd03c869b53f68d37dba
Alper Enver <alper.enver@emerson.com>
2015-06-29 15:35:20 -0500

    Added persistent connectors for scaling information associated with PLS model.

--- algorithms/PLS.py
+++ algorithms/PLS.py
@@ -1,10 +1,22 @@
-import numpy as np
 import pandas as pd
 import collections as coll
 import sys
 
 from sklearn.cross_decomposition import PLSRegression
 from FunctionBlock import FunctionBlock
+
+
+def x_y_mean_std(x, y):
+    x_mean = x.mean(axis=0)
+    y_mean = y.mean(axis=0)
+
+    x_std = x.std(axis=0, ddof=1)
+    x_std[x_std == 0.0] = 1.0
+
+    y_std = y.std(axis=0, ddof=1)
+    y_std[y_std == 0.0] = 1.0
+
+    return x_mean.tolist(), y_mean.tolist(), (x_std.tolist()), (y_std.tolist())
 
 
 class PLS(FunctionBlock):
@@ -46,12 +58,14 @@ class PLS(FunctionBlock):
             pls_coefficient = coll.OrderedDict(zip(x_df.columns.values, coefficients))
 
             model = list(pls_coefficient.items())
+            x_mean, y_mean, x_std, y_std = x_y_mean_std(x_values, y_values)
 
             pls_result = coll.OrderedDict()
-            pls_result['coefficients'] = model
-            pls_result['r squared'] = r2[0]
+            pls_result['Coefficients'] = model
+            pls_result['R-Squared'] = r2[0]
+            pls_result['MSE'] = float(ss_error_residual) / len(y_values)
 
-            data_dict = {'y vals': list(y_values[:, 0]), 'y pred': list(y_prediction[:, 0])}
+            data_dict = {'Y-Values': list(y_values[:, 0]), 'Y-Prediction': list(y_prediction[:, 0])}
 
             results_df = pd.DataFrame(data_dict)
 
@@ -59,12 +73,16 @@ class PLS(FunctionBlock):
             FunctionBlock.add_plot_result(self, results_df)
             FunctionBlock.add_general_results(self, pls_result)
             FunctionBlock.add_persisted_connector_result(self, 'model', model)
+            FunctionBlock.add_persisted_connector_result(self, 'x_mean', x_mean)
+            FunctionBlock.add_persisted_connector_result(self, 'x_std', x_std)
+            FunctionBlock.add_persisted_connector_result(self, 'y_mean', y_mean)
+            FunctionBlock.add_persisted_connector_result(self, 'y_std', y_std)
             FunctionBlock.save_all_results(self)
 
             FunctionBlock.report_status_complete(self)
 
-            return {FunctionBlock.getFullPath(self, 'model'): pls_model,
-                    FunctionBlock.getFullPath(self, 'ycomp'): (pd.DataFrame(data_dict))}
+            return {FunctionBlock.getFullPath(self, 'model'): model,
+                    FunctionBlock.getFullPath(self, 'y_comp'): results_df}
 
         except Exception as err:
             FunctionBlock.save_results(self)
