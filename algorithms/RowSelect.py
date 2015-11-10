7d6d8a6cb2db57bf7974e410a30523c9428d0248
Noel Bell <noel.bell@emerson.com>
2015-11-10 15:32:31 -0600

    RowSelect currently works for row index range to be deleted.   Timestamp range not working yet.

--- algorithms/RowSelect.py
+++ algorithms/RowSelect.py
@@ -2,6 +2,7 @@ import pickle
 import pandas as pd
 import json
 import sys
+import numpy as np
 
 from pymongo import MongoClient
 from datetime import datetime, timedelta
@@ -16,12 +17,12 @@ def json_to_query(json_str):
     time_ranges = []
     row_ranges = []
     for range in query["timeSelector"]:
-        start_time = parse(range["startTime"])
-        end_time = parse(range["endTime"])
-        time_ranges.append((start_time,end_time,))
+        start_time = range["startTime"]
+        end_time = range["endTime"]
+        time_ranges.append((start_time,end_time))
     for range in query["rowSelector"]:
-        start_row = parse(range["startRow"])
-        end_row = parse(range["endRow"])
+        start_row = range["startRow"]
+        end_row = range["endRow"]
         row_ranges.append((start_row, end_row))
     return time_ranges, row_ranges
 
@@ -37,12 +38,12 @@ class RowSelect(FunctionBlock):
 
             # ensure that parameters have been configured
 
-            FunctionBlock.check_connector_has_one_wire(self, 'In')
-
-            df = results_table[self.input_connectors['In'][0]]
-
+            FunctionBlock.check_connector_has_one_wire(self, 'in')
+            #print('before df')
+            df = results_table[self.input_connectors['in'][0]]
+            #print(' df......\n', df)
             query = str(self.parameters['Query'])
-
+            #print('query.......\n', query)
             if (query == 'None'):
                 FunctionBlock.report_status_configure(self)
                 return {FunctionBlock.getFullPath(self, 'out'): None}
@@ -50,6 +51,23 @@ class RowSelect(FunctionBlock):
             query = query.replace("'", "\"")
 
             time_ranges, row_ranges = json_to_query(query)
+
+            # First remove row_ranges if any
+            #df.index = pd.to_datetime(df.index)
+            print('first df.....\n', df)
+
+            for x,y in row_ranges:
+                print('x,y.....', int(x), int(y), type(int(x)), type(int(y)))
+                df = df.drop(df.index[int(x):int(y)])
+
+
+            # Second remove date_ranges
+            # for x,y in time_ranges:
+            #     print('x,y.....', x, y, type(x), type(y))
+            #     df.drop(df[np.datetime64(x):np.datetime64(y)], inplace=True)
+
+            #print(time_ranges)
+            #print(row_ranges)
 
             # save results and report block state is good
             FunctionBlock.save_results(self, df=df, statistics=True)
