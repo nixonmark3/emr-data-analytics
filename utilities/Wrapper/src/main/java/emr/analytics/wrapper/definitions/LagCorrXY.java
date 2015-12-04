ed4a88e8963f1eaa58ac957d979faa694858cbd4
NoelBell <noel.bell@emerson.com>
2015-12-04 16:21:58 -0600

    created wrapper for LagCorrXY similar to LagCorrelation except uses 2 data frames Y and X

---
+++ utilities/Wrapper/src/main/java/emr/analytics/wrapper/definitions/LagCorrXY.java
@@ -0,0 +1,65 @@
+package emr.analytics.wrapper.definitions;
+
+import emr.analytics.models.definition.*;
+import emr.analytics.wrapper.BlockDefinition;
+import emr.analytics.wrapper.IExport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LagCorrXY extends BlockDefinition implements IExport {
+
+    @Override
+    public Definition createDefinition() {
+
+        Definition definition = new Definition("LagCorr", "Lag Correlate", Category.TRANSFORMERS.toString());
+        definition.setDescription("Performs a lag correlation a given data frame");
+        return definition;
+    }
+
+    @Override
+    public ModeDefinition createOfflineMode(){
+
+        ModeDefinition modeDefinition = new ModeDefinition();
+        modeDefinition.setInputs(createInputConnectors());
+        modeDefinition.setOutputs(createOutputConnectors());
+        modeDefinition.setParameters(createParameters());
+
+        return modeDefinition;
+    }
+
+    @Override
+    public ModeDefinition createOnlineMode(){
+
+        return null;
+    }
+
+    public List<ConnectorDefinition> createInputConnectors() {
+
+        List<ConnectorDefinition> inputConnectors = new ArrayList<ConnectorDefinition>();
+        inputConnectors.add(new ConnectorDefinition("in", DataType.FRAME.toString()));
+        return inputConnectors;
+    }
+
+    public List<ConnectorDefinition> createOutputConnectors() {
+
+        List<ConnectorDefinition> outputConnectors = new ArrayList<ConnectorDefinition>();
+        outputConnectors.add(new ConnectorDefinition("out", DataType.FRAME.toString()));
+        outputConnectors.add(new ConnectorDefinition("summary_lag_corr", DataType.FRAME.toString()));
+        return outputConnectors;
+    }
+
+    public List<ParameterDefinition> createParameters() {
+
+        List<ParameterDefinition> parameters = new ArrayList<ParameterDefinition>();
+
+        parameters.add(new ParameterDefinition("MaxLag",
+                DataType.INT.toString(),
+                60,
+                new ArrayList<String>(),
+                null));
+
+        return parameters;
+    }
+
+}
